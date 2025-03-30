48d7da2bac3597666b837a95a23e45cbb95a7772
Isaac Sampaio <isaac.s.cruzz@gmail.com>
2025-03-30 18:43:30 -0300

    Fix bug deploy

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "../../components/header";
 import { LayoutContainer } from "./styles";
+import { Header } from "../../components/Header";
+
 
 export function DefaultLayout () {
   return (
